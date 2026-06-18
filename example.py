0250e729cb220eca6ab108733b7310cf3b214b5b
Kavindu Prabashwara <godlikeboot@gmail.com>
2026-06-18 07:47:59 +0530

    Initial Commit

---
+++ example.py
@@ -0,0 +1,28 @@
+#Print function to print the output
+print("Hello World")
+
+#use \n to add a new line
+print("Hello World ! \nHello World ! \nHello World !")
+
+print("Hello")
+print("Word")
+
+# is the same as:
+
+print("Hello\nWorld")
+
+# 2. String Concatenation
+# Use the plus symbol `+` to concatenate different strings together
+
+print("My name is" + " " + "Kavindu")
+
+# will output: "My name is Kavindu"
+
+#Multi line string in print function
+
+print("""Shopping List\n
+1. eggs
+2. Flour
+3. Butter
+5. Tomato
+6. Beef""")
