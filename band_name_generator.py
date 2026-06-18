75983689f96e309795efff7b606b467a2bdb2b03
Kavindu Prabashwara <godlikeboot@gmail.com>
2026-06-18 08:52:42 +0530

    project 1 added

---
+++ band_name_generator.py
@@ -0,0 +1,16 @@
+#Welcome screen
+print("""~~~~~~~~~~~~~~~~~~~~~~
+    Welcome To The 
+  Band Name Generator
+~~~~~~~~~~~~~~~~~~~~~~
+""")
+
+#Add Input from the user
+city = input("What city did you grew up in? ")
+pet = input("What is your pet's name? ")
+
+#Combine the name of their city and pet and show them their band name.
+print("""~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    Your Band Name Is : """ + city + " " + pet + """
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+""")
